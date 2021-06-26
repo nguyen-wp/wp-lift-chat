0f3fdc98c41183f149af6097c0479b4f28e3e7d1
Nguyen Pham <baonguyenyam@gmail.com>
2021-06-25 21:45:59 -0500

    update version

--- nguyen-app.php
+++ nguyen-app.php
@@ -16,7 +16,7 @@
  * Plugin Name:       @LIFT Creations - LIFT Chat
  * Plugin URI:        https://baonguyenyam.github.io/cv
  * Description:       A Better Way to Connect With Customers
- * Version:           2.2.3
+ * Version:           2.2.4
  * Author:            Nguyen Pham
  * Author URI:        https://baonguyenyam.github.io/cv
  * License:           GPL-2.0+
